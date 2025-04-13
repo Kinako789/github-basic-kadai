92ebd882b9f4c73af7ddd085c9fecce867d84146
Kinako789 <whitemaromaro@gmail.com>
2025-04-13 15:49:05 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,9 @@
+// 変数の宣言と値の代入
+let bodyWeight = 68; // 体重 (kg)
+let height = 1.7;     // 身長 (m)
+
+// BMIの計算式：[体重(kg)] ÷ [身長(m) × 身長(m)]
+let bmi = bodyWeight / (height * height);
+
+// 結果を出力
+console.log("BMIは " + bmi);
